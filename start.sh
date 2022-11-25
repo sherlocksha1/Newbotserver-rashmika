b5e1551847f895c0de46b1da43b7c9bff1cde010
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2022-11-25 21:39:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Localusersbot/Newbotserver-jd.git /JD-The-File-Donor
+  git clone https://github.com/Localusersbot/Newbotserver-jd.git /DQ-The-File-Donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /JD-The-File-Donor
+  git clone $UPSTREAM_REPO /DQ-The-File-Donor
 fi
 cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
