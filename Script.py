44bc931b305c07ec52c51e48f12107cde977ddd5
Localusersbot <115877356+Localusersbot@users.noreply.github.com>
2022-11-25 09:43:43 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,14 +3,14 @@ class script(object):
 Mʏ Nᴀᴍᴇ Is <a href=https://t.me/{}>{}</a>, I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇs, Jᴜsᴛ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ Aɴᴅ Eɴᴊᴏʏ 😍</b>"""
     HELP_TXT = """<b>Hᴇʏ {}
 Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Mʏ Cᴏᴍᴍᴀɴᴅs.</b>"""
-    ABOUT_TXT = """➪ 𝑴𝒚 𝒏𝒂𝒎𝒆 : {} 
+    ABOUT_TXT = """➪ 𝑴𝒚 𝒏𝒂𝒎𝒆 : <a href="https://t.me/at3moviesofficalbot">° 𝑱𝑫 °</a>
 ➪ 𝑪𝒓𝒆𝒂𝒕𝒐𝒓 : <a href="https://t.me/jdbotsupportgroup">ʜᴇʀᴇ</a>
 ➪ 𝑫𝒆𝒗𝒐𝒍𝒐𝒑𝒆𝒓 : <a href="https://t.me/AFxSU">ɛӼ_ռöֆ</a>
-➪ 𝑳𝒊𝒃𝒓𝒂𝒓𝒚 : 𝑷𝒚𝒓𝒐𝒈𝒓𝒂𝒎
-➪ 𝑳𝒂𝒏𝒈𝒖𝒂𝒈𝒆 : 𝑷𝒚𝒕𝒉𝒐𝒏 3
-➪ 𝑫𝒂𝒕𝒂 𝒃𝒂𝒔𝒆 : 𝑴𝒐𝒏𝒈𝒐𝑫𝑩
+➪ 𝑳𝒊𝒃𝒓𝒂𝒓𝒚 : <a href="https://www.program">𝑷𝒚𝒓𝒐𝒈𝒓𝒂𝒎</a>
+➪ 𝑳𝒂𝒏𝒈𝒖𝒂𝒈𝒆 : <a href="https://www.python.org/">𝑷𝒚𝒕𝒉𝒐𝒏 3</a>
+➪ 𝑫𝒂𝒕𝒂 𝒃𝒂𝒔𝒆 : <a href="https:// mongo db. Com">𝑴𝒐𝒏𝒈𝒐𝑫𝑩</a>
 ➪ 𝑩𝒐𝒕 𝒔𝒆𝒓𝒗𝒆𝒓 : <a href="https://t.me/share/url?url=Heroku%20https%3A//heroku.com/%0ARender%C2%A0https%3A//render.com/%0ARailway%C2%A0https%3A//railway.app/%0AKoyeb%20https%3A//koyeb.com/">ʜᴇʀᴇ</a>
-➪ 𝑩𝒖𝒊𝒍𝒅 𝑺𝒕𝒂𝒕𝒖𝒔 : v2.0.3 [ 𝑺𝒕𝒂𝒃𝒍𝒆 ]</b>"""
+➪ 𝑩𝒖𝒊𝒍𝒅 𝑺𝒕𝒂𝒕𝒖𝒔 : <a href="https://status">v2.0.3 [ 𝑺𝒕𝒂𝒃𝒍𝒆 ]</b>"""
     SOURCE_TXT = """<b>ɴᴏᴛᴇ:
 - ᴛʜɪꜱ ʙᴏᴛ ɪꜱ ᴀɴ ᴏᴘᴇɴ ꜱᴏᴜʀᴄᴇ ᴘʀᴏᴊᴇᴄᴛ.
 - ꜱᴏᴜʀᴄᴇ - <a href="https://t.me/AdhavaaBiriyaniKittiyalo">ʜᴇʀᴇ</a>
