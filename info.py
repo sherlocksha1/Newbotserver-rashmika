9d9fabc4e2ef46ac49e4ae11aa3c5f38fbbb918d
Localusersbot <115877356+Localusersbot@users.noreply.github.com>
2022-11-26 14:14:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,7 +46,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/jdbotsupportgroup')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/bots_supported')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+ip38nA3Tm0A3MGRl')
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
